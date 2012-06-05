537a39bb93a68fcf7237b5c902ccc953c5328484
Saul Hardman <hello@iamsaul.co.uk>
2012-06-05 18:09:34 +0100

    Set cookie timeout to 0

--- application/config/config.php
+++ application/config/config.php
@@ -224,7 +224,7 @@ $config['cache_path'] = '';
 | MUST set an encryption key.  See the user guide for info.
 |
 */
-$config['encryption_key'] = 'atweeviate';
+$config['encryption_key'] = 'L50YW9dL3lybxaolz833meLM99010wCn';
 
 /*
 |--------------------------------------------------------------------------
@@ -245,7 +245,7 @@ $config['encryption_key'] = 'atweeviate';
 |
 */
 $config['sess_cookie_name']		= 'ci_session';
-$config['sess_expiration']		= 7200;
+$config['sess_expiration']		= 0;
 $config['sess_expire_on_close']	= FALSE;
 $config['sess_encrypt_cookie']	= FALSE;
 $config['sess_use_database']	= FALSE;
