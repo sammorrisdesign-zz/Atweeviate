d82d7cf7cd6d43948b2efee199b3220efc9e86cd
Saul Hardman <hello@iamsaul.co.uk>
2012-06-05 19:31:50 +0100

    Added Curl library and did a simple get using API Url

--- application/controllers/atweeviate.php
+++ application/controllers/atweeviate.php
@@ -27,7 +27,7 @@ class Atweeviate extends CI_Controller {
 	 */
 	public function getRequestUrl($word = '')
 	{
-		$url = sprintf('%s/%s/%s', $this->api_url, $this->api_key, $word);
+		$url = sprintf('%s/%s/%s/', $this->api_url, $this->api_key, $word);
 
 		return $url;
 	}
